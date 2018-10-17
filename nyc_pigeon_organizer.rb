1406e08d21a4252ad52515262d46171234705359
Sung Bae <singsang2@gmail.com>
2018-10-17 15:41:42 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,29 +20,29 @@ def nyc_pigeon_organizer(data)
     end
     pigeon_list[name] = temp_hash 
   end
-  puts pigeon_list
+  #puts pigeon_list
   
   
   
 end
 
-data = {
-        :color => {
-          :purple => ["Theo", "Peter Jr.", "Lucky"],
-          :grey => ["Theo", "Peter Jr.", "Ms. K"],
-          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-          :brown => ["Queenie", "Alex"]
-        },
-        :gender => {
-          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-          :female => ["Queenie", "Ms. K"]
-        },
-        :lives => {
-          "Subway" => ["Theo", "Queenie"],
-          "Central Park" => ["Alex", "Ms. K", "Lucky"],
-          "Library" => ["Peter Jr."],
-          "City Hall" => ["Andrew"]
-        }
-}
+# data = {
+#         :color => {
+#           :purple => ["Theo", "Peter Jr.", "Lucky"],
+#           :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#           :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#           :brown => ["Queenie", "Alex"]
+#         },
+#         :gender => {
+#           :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#           :female => ["Queenie", "Ms. K"]
+#         },
+#         :lives => {
+#           "Subway" => ["Theo", "Queenie"],
+#           "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#           "Library" => ["Peter Jr."],
+#           "City Hall" => ["Andrew"]
+#         }
+# }
 
-nyc_pigeon_organizer(data)
+# nyc_pigeon_organizer(data)
